b69af77a4669e1c76207ef7bf690088e410d5584
Achille <simachille@yahoo.fr>
2018-06-20 22:44:33 +0200

    MAJ fichiers

--- discovery/src/main/java/com/lm/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/lm/discovery/DiscoveryApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-	@EnableEurekaServer
-	@SpringBootApplication
-	public class DiscoveryApplication {
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApplication {
 
-		public static void main(String[] args) {
-			SpringApplication.run(DiscoveryApplication.class, args);
-		}
-	}
+public static void main(String[] args) {
+	SpringApplication.run(DiscoveryApplication.class, args);
+}
+}
 
 
